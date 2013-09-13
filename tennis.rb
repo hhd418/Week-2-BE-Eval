c573520cf6e373d50449de1c5684303c2a71279a
MakerSquare Student <students@themakersquare.com>
2013-09-13 12:02:10 -0500

    Final commit. Did some refactoring with a few private methods. Finished project with extensions!!!

--- tennis.rb
+++ tennis.rb
@@ -53,24 +53,7 @@ module Tennis
       return 'thirty' if @points == 2
       return 'duece' if @points == 3 && @opponent.points == 3
       return 'forty' if @points == 3
-      if @points == 4 
-        if @opponent.points < 3
-          @games_won += 1
-          @points = 0
-          @opponent.points = 0
-          set_won?
-          match_won?
-          return 'win'
-        elsif @opponent.points == 3
-          @points -= 1
-          @opponent.points -= 1
-          return 'advantage'
-        elsif @opponent.points == 4
-          @points -= 1
-          @opponent.points -= 1
-          return 'duece'
-        end
-      end
+      game_won?
     end
 
     private
@@ -90,6 +73,8 @@ module Tennis
       end
     end
 
+    # Check to see if a match has been won
+    # Players must get to 3 sets to win the match
     def match_won?
       if @sets_won == 3
         @match_won = 1
@@ -101,7 +86,30 @@ module Tennis
         @sets_won = 0
         @opponent.sets_won = 0
       end
+    end
 
+    # Check to see if a game has been won
+    # Players can only win a game if they have gotten to 4 and are up by 2
+    # If they get to 3 - 2 or 4 - 3, a point is deducted from each to allow them to get to 4 2
+    def game_won?
+      if @points == 4
+        if @opponent.points < 3
+          @games_won += 1
+          @points = 0
+          @opponent.points = 0
+          set_won?
+          match_won?
+          return 'win'
+        elsif @opponent.points == 3
+          @points -= 1
+          @opponent.points -= 1
+          return 'advantage'
+        elsif @opponent.points == 4
+          @points -= 1
+          @opponent.points -= 1
+          return 'duece'
+        end
+      end
     end
 
   end
